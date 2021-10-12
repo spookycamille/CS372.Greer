a05bde5c1648bb409b67a06e8a63c414fa41d834
spookycamille <89113571+spookycamille@users.noreply.github.com>
2021-10-12 17:39:46 -0500

    Greer Assignment 4

---
+++ apps/assignment4/Assignment4.1/Assignment4.1/Assignment4.1.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+
+using namespace std;
+
+int main()
+{
+	int choice;
+	string fileText;
+	string line;
+
+	cout << "Enter '1' to display all items" << endl;
+	cout << "Enter '2' to display a selected item" << endl;
+	cout << "Enter '3' to mark a task as done" << endl;
+	cout << "Enter '4' to exit the program" << endl;
+
+	cin >> choice;
+
+	ofstream file;
+	file.open("Asg4.1.txt");
+
+	ifstream readFile("Asg4.1.txt");
+
+	switch (choice)
+	{
+	case 1:
+		//Display code
+		while (getline(readFile, fileText))
+		{
+			cout << fileText;
+		}
+		break;
+
+	case 2:
+		//Display slected item code
+		for (int lineno = 1; getline(readFile, line) && lineno < 3; lineno++)
+		{
+			if (lineno == 2)
+			{
+				cout << line << endl;
+			}
+		}
+		break;
+
+	case 3:
+		//Mark task
+		break;
+
+	case 4:
+		cout << "End of program" << endl;
+		return 0;
+		break;
+
+	default:
+		cout << "Invlaid choice. Enter a number between 1-4" << endl;
+		break;
+	}
+
+	file.close();
+}
---
+++ apps/assignment4/Assignment4.2/Assignment4.2/Assignment4.2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+
+using namespace std;
+
+int main()
+{
+	ofstream file;
+	file.open("Asg4.2.txt");
+}
